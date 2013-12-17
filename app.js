48523606d1181f3b3688df1121f3632834330aee
Paulo McNally <paulomcnally@gmail.com>
2013-12-17 16:20:23 -0600

    add newrelic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var newrelic = require('newrelic');
 
 var controllers = require('./controllers');
 
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Lo que debes saber'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '396124d24b27d67212f6227f9c158c78f36f68ba',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
