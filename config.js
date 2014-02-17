dd014ffc5a3ab09b3dd73fb800d0e9502c004f72
Paulo McNally <paulomcnally@gmail.com>
2014-02-17 17:01:58 -0600

    replace path and remvoe memcached

--- config.js
+++ config.js
@@ -15,14 +15,6 @@ exports.web = web;
 
 var repository = {};
 
-repository.exec = '/var/www/html/loquedebessaber.com/loquedebessaber_content/post-receive.sh';
+repository.exec = '/sites/loquedebessaber.com/loquedebessaber_content/post-receive.sh';
 
 exports.repository = repository;
-
-var memcached = {};
-memcached.username = '98754e';
-memcached.password = '93075757b8';
-memcached.server = 'mc4.dev.ec2.memcachier.com';
-memcached.port = '11211';
-
-exports.memcached = memcached;
