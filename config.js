4d4ce19213eac8b4830a6c0598a11230686a00c4
Paulo McNally <paulomcnally@gmail.com>
2014-02-19 02:04:00 -0600

    reestructure config

--- config.js
+++ config.js
@@ -1,42 +1,44 @@
-// content = articles, authors, news, etc
-var content = {};
-content.repository = {};
-content.repository.url = 'https://github.com/Eyling/loquedebessaber_content.git';
-content.repository.name = 'loquedebessaber_content';
-content.domain = 'http://loquedebessaber.com/';
-
-exports.content = content;
-
-var web = {};
-
-web.title = 'Lo que debes saber';
-
-exports.web = web;
-
-var repository = {};
-
-repository.exec = 'sh /sites/loquedebessaber.com/loquedebessaber_content/post-receive.sh';
-
-exports.repository = repository;
-
-var rss = {
-    title:          'Lo que debes saber',
-    description:    'Blog enfocado en un sentido humanista.',
-    link:           'http://loquedebessaber.com/',
-    image:          'http://loquedebessaber.com/img/logo.png',
-    copyright:      'Todos los derechos reservados 2014, Eyling Montenegro',
-
-    author: {
-        name:       'Eyling Montenegro'
+function config(){
+    // repository and domain data
+    this.content = {
+        repository: {
+            url: 'https://github.com/Eyling/loquedebessaber_content.git',
+            name: 'loquedebessaber_content'
+        },
+        domain: 'http://loquedebessaber.com/'
     }
+
+    // web data
+    this.web =  {
+        title: 'Lo que debes saber'
+    }
+
+    // repository
+    this.repository = {
+        exec: 'sh /sites/loquedebessaber.com/loquedebessaber_content/post-receive.sh'
+    }
+
+    // rss
+    this.rss = {
+        title:          this.web.title,
+            description:    'Blog enfocado en un sentido humanista.',
+            link:           this.content.domain,
+            image:          this.content.domain + 'img/logo.png',
+            copyright:      'Todos los derechos reservados 2014, Eyling Montenegro',
+            author: {
+            name:       'Eyling Montenegro'
+        }
+    }
+
+    // links
+    this.links = {
+        article: 'articulo'
+    }
+
+    this.lang = 'es'
+
 }
 
-exports.rss = rss;
+var config = new config();
 
-var links = {
-    article: 'articulo'
-}
-
-exports.links = links;
-
-exports.lang = 'es';
+module.exports = config;
